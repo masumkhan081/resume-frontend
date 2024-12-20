2e9194f8a9306bae5feeb63343b84cf030ee471d
masumkhan081 <masumkhan081@gmail.com>
2024-12-20 22:05:22 +0600

    init , react to next

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       spacing: {
